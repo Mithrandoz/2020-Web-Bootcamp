c1152cf58e41b573e0e20b10b0950b0ac7feeac7
Mithrandoz <61643616+Mithrandoz@users.noreply.github.com>
2020-04-16 17:35:52 -0500

    Completed sections 16 and 17

---
+++ Section 17/intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var supervillan = require("supervillains")
+var mySuper = superheroes.random();
+var myVill = supervillan.random();
+console.log(mySuper);
+console.log(myVill);
+
