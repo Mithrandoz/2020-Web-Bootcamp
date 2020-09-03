37cc3fa63c8fda3acd88896f9d26490efc1d3236
Mithrandoz <61643616+Mithrandoz@users.noreply.github.com>
2020-09-03 18:04:51 -0500

    completed Section 22, halfway though Section 23

--- Section_22/todolist-v1/app.js
+++ Section_22/todolist-v1/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
 // item is logged as empty to prevent errors
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,29 +17,15 @@ app.use(express.static("Public"));
 
 app.get("/", function (req, res) {
     
-    let today = new Date();
-    let currentDay = today.getDay();
+    const day = date.getDate();
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
-    // day and item is rendered sucessfully
     res.render("list", {listTitle: day, newItem: items});
-    // app.get is run successfully
-    console.log("ran")
-    // logged successfully
-    console.log(items)
 
 });
 
 app.post("/", function(req, res) {
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
         workItems.push(item);
